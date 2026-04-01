3b26a0c2c8aad50aa3f1b322774f270e1c019255
Dan <dan@eighttrigrams.net>
2026-04-01 21:26:03 +0100

    Format mail nicely

--- src/clj/et/blog/mail.clj
+++ src/clj/et/blog/mail.clj
@@ -1,5 +1,6 @@
 (ns et.blog.mail
-  (:require [postal.core :as postal]))
+  (:require [postal.core :as postal]
+            [et.blog.render :as render]))
 
 (defn- smtp-config []
   {:host (or (System/getenv "SMTP_HOST") "w00cbd36.kasserver.com")
@@ -7,6 +8,13 @@
    :tls true
    :user (System/getenv "SMTP_USER")
    :pass (System/getenv "SMTP_PASSWORD")})
+
+(defn- build-html [title subtitle post-content article-url]
+  (str "<h1>" title "</h1>"
+       (when (and subtitle (not= subtitle ""))
+         (str "<p><em>" subtitle "</em></p>"))
+       (render/markdown->html post-content)
+       "<p><a href=\"" article-url "\">" article-url "</a></p>"))
 
 (defn send-article-notification! [subscribers title subtitle post-content article-url]
   (let [config (smtp-config)]
@@ -17,11 +25,8 @@
             {:from "dan@eighttrigrams.net"
              :to email
              :subject (str "New article: " title)
-             :body (str title
-                        (when (and subtitle (not= subtitle ""))
-                          (str "\n" subtitle))
-                        "\n\n" post-content
-                        "\n\n" article-url)})
+             :body [{:type "text/html; charset=utf-8"
+                      :content (build-html title subtitle post-content article-url)}]})
           (println (str "Sent notification to " email))
           (catch Exception e
             (println (str "Failed to send to " email ": " (.getMessage e)))))))))
